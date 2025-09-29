89aef22964019b26f5f30df0b49a4abb641e1d17
Navya <navya151205@gmail.com>
2025-09-29 13:00:21 +0530

    Fetch API

--- CSS/Flexwork/gridwork/JavaScript/DOM/index.js
+++ CSS/Flexwork/gridwork/JavaScript/DOM/index.js
@@ -1,63 +1,115 @@
-// const div = document.getElementsByTagName('div');
-// div[0].innerText = "hello world";
-// div[0].style.color ='red';
-// div[0].style.backgroundColor = 'cyan';
+// const div=document.getElementsByTagName('div');
+
+// div[0].innerText="Hello World";
+// div[0].style.color='red';
+// div[0].style.backgroundColor='cyan';
 // console.log(div);
+// const container=document.getElementsByClassName('container');
 
-const container = document.getElementsByClassName('container');
-
-// container[0].innerHTML = '<h2 style=color:red > hello user </h2>';
+// container[0].innerHTML='<h2 style=color:red>Hello user</h2>';
 // console.dir(container);
-
-// const h2 = document.createElement('h2'); //creating dynamic elements through DOM
-// h2.innerText = "ABES Engineering College";
+// const h2=document.createElement('h2');
+// h2.innerText="Abes Engineering College";
 // h2.style.color = "white";
 // h2.style.backgroundColor = "brown";
 // console.log(h2);
 // container[0].appendChild(h2);
-
-
-
-// const button = document.getElementById('btn');
+// const img=document.createElement('img');
+// img.src='WhatsApp Image 2025-09-12 at 12.52.29_884232f4.jpg';
+// img.setAttribute('height','200px');
+// img.setAttribute('width','200px');
+// console.log(img);
+// container[0].appendChild(img);
+// const button=document.getElementById('btn');
 // console.log(button);
-// document.getElementById('disp').innerHTML = "<h2> loading...</h2>"
 
 // function msg(){
-//     // console.log("hi, im working on DOm");
-//     const img =document.createElement('img');
-//     img.src="https://img.freepik.com/free-photo/laptop-with-sun-background_1232-429.jpg?semt=ais_incoming&w=740&q=80";
-
-//     img.setAttribute('height','200px');
-//     img.setAttribute('width','300px');
-//     console.log(img);
-//     document.getElementById('disp').innerHTML = "<h2> welcome to ABES Engineering College </h2>";
-//     container[0].appendChild(img);
-//     // alert("Hi, I am working on DOM");
+//     document.getElementById('disp').innerHTML="<h>Loading image...</h>"
+//     setTimeout(()=>{
+// const img=document.createElement('img');
+// img.src='WhatsApp Image 2025-09-12 at 12.52.29_884232f4.jpg';
+// img.setAttribute('height','200px');
+// img.setAttribute('width','200px');
+// console.log(img);
+// document.getElementById('disp').innerHTML="<h2>Welcome to Abes Engineering College</h2>";
+// container[0].appendChild(img);
+// },2000);
+   
+    //alert("hii.. I am using Dom");
 // }
 // button.addEventListener('click',msg);
 
-
-// Blocking 
-// const button = document.getElementById('btn');
+// const button=document.getElementById('btn'); Blocking code#####
 // function longData(){
 //     console.log("Start");
-//     for(i=0;i<100000000;i++){
+//     for(i=0;i<100000;i++){
 //         console.log(i);
 //     }
 //     console.log("End");
 // }
 // button.addEventListener('click',longData);
 
-// Callback function
-function greetmsg(msg){
-    return "Welcome to abes college"+msg;
-}
-function msgHandler(msg,clbk){
-    console.log(msg);
-    const data=clbk(msg);
-    return data;
-}
-const completmsg=msgHandler("Hii",grretmsg);
-console.log(completmsg);
+// console.log("start") ##### start end welcome to fsd
+// setTimeout(()=>{
+//     console.log("Welcome to FSD")
+// },2000)
+// console.log("end")
 
+// function greetmsg(myname){
+//     return "Hiii "+myname+" Welcome to abes college";
+// }
+// function msgHandler(msg,clbk){
+//    console.log(msg);
+//    const data=clbk(msg);
+//    return data;
+// }
+// const student=["Rahul","Anant","Vansh"];
+//     student.forEach((myname)=>{
+//     console.log(msgHandler(myname,greetmsg))
+// })
+// const completemsg=msgHandler("hii",greetmsg);
+// console.log(completemsg)
 
+//PROMISE
+
+// const mypromise=new Promise((resolve,reject)=>{
+//     const password='786mish@';
+//     if(password.length>8){
+//         resolve("Password length is ok");
+//     }
+//     else{
+//         reject("Pasword length is not as per our policy");
+//     }
+// });
+// mypromise.then((result)=>{console.log(result)})
+// .catch((err)=>(console.log(err)))
+// .finally(()=>{
+//     console.log("All the resources have closed successfully");
+// })
+
+// async function handleData(){
+//     const data=await mypromise;
+//     console.log(data)
+// }
+// handleData();
+
+const button=document.getElementById('btn');
+const disp=document.getElementById('disp');
+let table='<table border=1>';
+async function fetchData(){
+    disp.innerText="<h2>Loading...</h2>";
+    const response=await fetch('https://dummyjson.com/recipes');
+    const jsonData=await response.json();
+    console.log(jsonData.recipes[0].name);
+    // disp.innerHTML=<h2>${jsonData.recipes[0].name}</h2>;
+    jsonData.recipes.forEach(element=>{
+        table+=`<tr>
+        <td><img src=${element.image} height=200 width=200 alt='image'></td>
+        <td>${element.id}</td>
+        <td>${element.name}</td>
+        </tr>`
+    });
+    table+='</table>';
+    disp.innerHTML=table;
+}
+button.addEventListener('click',fetchData);
---
+++ CSS/Flexwork/gridwork/JavaScript/index.js
@@ -0,0 +1,74 @@
+// console.log("Hello World");
+// var a=20;
+// console.log("a"+a);
+
+// console.log(a);
+// var a=20;
+
+// var a=50;
+// if(a>10){
+//     let a=100;
+//     console.log("a="+a);
+// }else{
+//     console.log("hhiiii")
+// }
+// console.log("hiii"+a);
+
+// function fun(msg){
+//     return "Hey...";
+//     console.log("Hello");
+// }
+// const data=fun("greeting....");
+// console.log(data);
+
+// function as an expersiion
+// const data=function(a,b){
+//     return a*b;
+// }
+// console.log(data(12,20));
+
+// Arrow function
+// const data=()=>{
+//     console.log("hey..using arrow function")
+// }
+// data("in react development");
+
+// const data=msg=>msg;
+// const output=data("Greeting of the day");
+// console.log(output);
+
+// IIFE
+// (function(){
+//     console.log("Welcome to MERN session")
+// })();
+
+// setTimeout(() => {
+//   console.log("Hiiii....")
+// },1000);
+
+// function greet(msg="Java"){
+//     console.log(msg+" is a good language");
+// }
+// greet("python");
+
+// function selectlanguage(lang){
+//     let result;
+//     if(lang=="java"){
+//         function javaComplier(){
+//             return "Using java Compiler"
+//         }
+//         result=javaCompiler();
+//     }
+//     else if(lang=='c'){
+//         function cCompiler(){
+//             return "Using c Compiler"
+//         }
+//         result=cCompiler();
+//     }
+//     else{
+//         result="No compiler found"
+//     }
+//     return result;
+// }
+// const data=selectlanguage("java");
+// console.log(data);
